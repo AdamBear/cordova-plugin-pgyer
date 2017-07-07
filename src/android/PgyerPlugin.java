ce04d16f8be9d8093fa2875690cd8ca03a45a6a1
adambear <adamxiong@126.com>
2017-07-07 16:31:29 +0800

    fix wrong import

--- src/android/PgyerPlugin.java
+++ src/android/PgyerPlugin.java
@@ -2,12 +2,16 @@ package wang.imchao.plugin;
 
 import android.util.Log;
 
+import com.pgyersdk.feedback.PgyFeedback;
 import com.pgyersdk.feedback.PgyFeedbackShakeManager;
 import com.pgyersdk.update.PgyUpdateManager;
 
+import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class PgyerPlugin extends CordovaPlugin {
     private static String TAG = "PgyerPlugin";
